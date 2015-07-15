b4ca9b2c9f98bb161bfe585337fc4f84bf307510
LiuHaoyu76 <liuhaoyu76@outlook.com>
2015-07-15 19:38:02 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/lhy_pc/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/lhy_pc/myapplication2/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1asd23
+        //1asdqq23
     }
 
     @Override
